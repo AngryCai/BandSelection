6614180792e8136d6f3730878de511625c5b8766
AngryCai <caiyaomxc@outlook.com>
2018-03-07 09:41:59 +0800

    construct framework

---
+++ classes/utility.py
@@ -0,0 +1,24 @@
+"""
+Description:
+    auxiliary functions
+"""
+from Toolbox.Preprocessing import Processor
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.preprocessing import maxabs_scale
+from sklearn.metrics import accuracy_score
+
+
+def eval_band(new_img, gt, train_inx, test_idx):
+    p = Processor()
+    img_, gt_ = p.get_correct(new_img, gt)
+    img_ = maxabs_scale(img_)
+    # X_train, X_test, y_train, y_test = train_test_split(img_, gt_, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = img_[train_inx], img_[test_idx], gt_[train_inx], gt_[test_idx]
+    knn_classifier = KNN(n_neighbors=5)
+    knn_classifier.fit(X_train, y_train)
+    # score = cross_val_score(knn_classifier, img_, y=gt_, cv=3)
+    y_pre = knn_classifier.predict(X_test)
+    score = accuracy_score(y_test, y_pre)
+    # score = np.mean(score)
+    return score
--- demo/test.py
+++ demo/test.py
@@ -1,7 +1,11 @@
 from Toolbox.Preprocessing import Processor
 from sklearn.preprocessing import minmax_scale
-from BandSelection.classes.DSC_NET import DSC_BandSelection
+from BandSelection.classes.DSC_NET import DSC_NET
 import numpy as np
+from BandSelection.classes.DSC_NET import DSCBS
+from BandSelection.classes.SPEC import SPEC_HSI
+from BandSelection.classes.utility import eval_band
+
 
 if __name__ == '__main__':
     root = 'F:\\Python\\HSI_Files\\'
@@ -20,22 +24,20 @@ if __name__ == '__main__':
     # Img, Label = Img[:256, :, :], Label[:256, :]
     n_row, n_column, n_band = img.shape
     train_inx, test_idx = p.get_tr_tx_index(p.get_correct(img, gt)[1], test_size=0.9)
-    img_train = minmax_scale(img.reshape(n_row * n_column, n_band)).reshape((n_row, n_column, n_band))
-    img_train = np.transpose(img_train, axes=(2, 0, 1))  # Img.transpose()
-    img_train = np.reshape(img_train, (n_band, n_row, n_column, 1))
+    X_img = minmax_scale(img.reshape(n_row * n_column, n_band)).reshape((n_row, n_column, n_band))
+
+    n_selected_band = 10
 
     n_input = [n_row, n_column]
     kernel_size = [11]
-    n_hidden = [16]*2
+    n_hidden = [16]
     batch_size = n_band
-    model_path = './pretrain-model-Indian/model.ckpt'
-    ft_path = './pretrain-model-Indian/model.ckpt'
-    logs_path = './pretrain-model-Indian/logs'
+    model_path = './pretrain-model-COIL20/model.ckpt'
+    ft_path = './pretrain-model-COIL20/model.ckpt'
+    logs_path = './pretrain-model-COIL20/logs'
 
     num_class = 5  # how many class we sample
     batch_size_test = n_band
-
-
 
     iter_ft = 0
     ft_times = 50
@@ -45,33 +47,16 @@ if __name__ == '__main__':
 
     reg1 = 1e-4
     reg2 = 150.0
+    kwargs = {'n_input': n_input, 'n_hidden': n_hidden, 'reg_const1': reg1, 'reg_const2': reg2,
+              'kernel_size': kernel_size, 'batch_size': batch_size_test, 'model_path': model_path,
+              'logs_path': logs_path}
 
-    dsc_bs = DSC_BandSelection(n_input=n_input, n_hidden=n_hidden, reg_const1=reg1, reg_const2=reg2, kernel_size=kernel_size,
-                               batch_size=batch_size_test, model_path=model_path, logs_path=logs_path)
+    algorithm = [SPEC_HSI(n_selected_band),
+                 DSCBS(n_selected_band, **kwargs)
+                 ]
 
-    acc_ = []
-    all_loss = []
-    all_acc = []
-    for i in range(0, 1):
-        # coil20_all_subjs = copy.deepcopy(Img)
-        # coil20_all_subjs = coil20_all_subjs.astype(float)
-        # label_all_subjs = copy.deepcopy(Label)
-        # label_all_subjs = label_all_subjs - label_all_subjs.min() + 1
-        # label_all_subjs = np.squeeze(label_all_subjs)
-        dsc_bs.initlization()
-        # CAE.restore()
-        for iter_ft in range(ft_times):
-            iter_ft = iter_ft + 1
-            C, l1_cost, l2_cost, total_loss = dsc_bs.finetune_fit(img_train, learning_rate)
-            all_loss.append(total_loss)
-            if iter_ft % display_step == 0:
-                print("epoch: %.1d" % iter_ft,
-                      "L1 cost: %.8f, L2 cost: %.8f, total cost: %.8f" % (l1_cost, l2_cost, total_loss))
-                C = dsc_bs.thrC(C, alpha)
-                y_x, CKSym_x = dsc_bs.post_proC(C, num_class, 1, 4)
-                bands = dsc_bs.band_selection(y_x, img)  # n_row * n_clm * n_class
-                score = dsc_bs.eval_band(bands, gt, train_inx, test_idx)
-                all_acc.append(score)
-                print('eval score:', score)
-        print(all_loss)
-        print(all_acc)
+    for alg in algorithm:
+        alg.fit(X_img)
+        X_new = alg.predict(X_img)
+        acc = eval_band(X_new, gt, train_inx, test_idx)
+        print(acc)
